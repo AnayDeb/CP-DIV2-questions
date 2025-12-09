d026123d85c6e05bbc55fcb65d042bba0474c0c4
Anay <debanay1@gmail.com>
2025-12-09 22:59:50 +0530

    Was a observation based question learnt something new

---
+++ Deque_Process.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios::sync_with_stdio(false); cin.tie(nullptr);
+
+#define int long long
+#define pb push_back
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define rep(i,a,b) for(int i = (a); i < (b); i++)
+
+// ===== GCD =====
+int gcd(int a, int b) {
+    return (b == 0 ? a : gcd(b, a % b));
+}
+
+// ===== LCM =====
+int lcm(int a, int b) {
+    return (a / gcd(a, b)) * b;
+}
+
+// ===== PRIME CHECK =====
+bool isPrime(int n) {
+    if(n < 2) return false;
+    if(n % 2 == 0) return n == 2;
+    for(int i = 3; i * i <= n; i += 2)
+        if(n % i == 0) return false;
+    return true;
+}
+
+// ===== SOLVE FUNCTION =====
+void solve() {
+	int n; cin>>n;
+	vi a(n);
+	rep(i,0,n) cin>>a[i];
+	int i=0,j=n-1;
+	string ans="";
+	int k=0;
+	while(i<=j){
+		if(k&1){
+			if(a[i]>=a[j]){
+				ans+='L';
+				i++;
+				k++;
+			}
+			else {ans+='R'; k++; j--;}
+		}
+		else{
+			if(a[i]<=a[j]) {ans+='L'; k++; i++;}
+			else {ans+='R'; k++; j--;}
+		}
+	}
+	cout<<ans<<endl;
+}
+
+int32_t main() {
+    fast;
+
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
+
