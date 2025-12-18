a25436837168e50d746dd3b525fd6aa35e264fb6
Anay <debanay1@gmail.com>
2025-12-18 18:18:41 +0530

    Was a good stl based question learnt new things

---
+++ Largebags.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios::sync_with_stdio(false); cin.tie(nullptr);
+
+#define int long long
+#define pb push_back
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define rep(i,a,b) for(int i = (a); i < (b); i++)
+
+// ===== GCD =====
+int gcd(int a, int b) {
+    return (b == 0 ? a : gcd(b, a % b));
+}
+
+// ===== LCM =====
+int lcm(int a, int b) {
+    return (a / gcd(a, b)) * b;
+}
+
+// ===== PRIME CHECK =====
+bool isPrime(int n) {
+    if(n < 2) return false;
+    if(n % 2 == 0) return n == 2;
+    for(int i = 3; i * i <= n; i += 2)
+        if(n % i == 0) return false;
+    return true;
+}
+
+// ===== SOLVE FUNCTION =====
+void solve() {
+	int n; cin>>n;
+	vi a(n);
+	map<int,int>freq;
+	set<int>bag;
+	rep(i,0,n) {
+		cin>>a[i];
+		bag.insert(a[i]);
+		freq[a[i]]+=1;
+	}
+	while(!bag.empty()){
+		int mini=*bag.begin();
+		if(freq[mini]==1){
+			cout<<"No"<<endl;
+			return;
+		}
+		freq[mini+1]+=freq[mini]-2;
+		bag.erase(mini);
+		if(freq[mini+1]){
+			bag.insert(mini+1);
+		}
+		freq[mini]=0;
+	}
+	cout<<"Yes"<<endl;
+}
+
+int32_t main() {
+    fast;
+
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
+
